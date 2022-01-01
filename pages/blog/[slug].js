e7d5a510ec0e9739a6769c1d3770b47acbc193f6
Dave Beidle <dbeidle@gmail.com>
2022-01-01 11:49:24 -0600

    added meta tags for image and description

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -5,11 +5,13 @@ import { getAllPosts } from "../../lib/data";
 import getDate from "../../lib/getDate";
 import remarkPrismPlugin from "remark-prism";
 
-export default ({ title, date, content }) => {
+export default ({ title, date, content, hero, summary }) => {
   return (
     <div className=" w-3/4 lg:w-1/2 px-10 h-full overflow-scroll m-auto text-sb-norm border-sb-dark border-solid border-2 rounded-lg bg-sb-light shadow p-3 mt-20 ">
       <Head>
         <title>{title}</title>
+        <meta name="description" content={summary} />
+        <meta property="og:image" content={hero} />
       </Head>
       <div className="text-sb-dark border-b-2 border-sb-dark border-opacity-75 border-solid pb-2">
         <h1 className="text-4xl font-medium ">{title}</h1>
